32c50755f40f6e42f95334f795e5caf8cfd68a12
TemimaFogel <83607694+TemimaFogel@users.noreply.github.com>
2026-03-17 18:58:32 +0200

    Update CorsConfig.java

--- server/src/main/java/com/dailychallenge/config/CorsConfig.java
+++ server/src/main/java/com/dailychallenge/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "https://https://daily-challenge-vwgy.onrender.com/",
+                                "https://daily-challenge-vwgy.onrender.com/",
                                 "http://localhost:5173"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
